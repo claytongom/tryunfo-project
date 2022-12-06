675813a4192e29890099b07bfbc350eb92d503a9
claytongom <claytoncnx@gmail.com>
2022-12-06 10:54:40 -0300

    resolução requisito 5

--- src/App.js
+++ src/App.js
@@ -17,8 +17,36 @@ class App extends React.Component {
   };
 
   onInputChange = ({ target }) => {
-    const { name, value } = target;
-    this.setState({ [name]: value });
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [name]: value }, this.validationButton);
+  };
+
+  validationButton = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    } = this.state;
+
+    const aboveNinety = 90;
+    const totalAttr = 210;
+
+    const validAttr1 = Number(cardAttr1) >= 0 && Number(cardAttr1) <= aboveNinety;
+    const validAttr2 = Number(cardAttr2) >= 0 && Number(cardAttr2) <= aboveNinety;
+    const validAttr3 = Number(cardAttr3) >= 0 && Number(cardAttr3) <= aboveNinety;
+    const sum = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) <= totalAttr;
+    const validCard = cardName !== '' && cardDescription !== '' && cardImage !== '';
+
+    this.setState({ isSaveButtonDisabled: !(validAttr1
+      && validAttr2
+      && validAttr3
+      && sum
+      && validCard),
+    });
   };
 
   render() {
@@ -37,4 +65,4 @@ class App extends React.Component {
   }
 }
 
-export default App; //
+export default App;
